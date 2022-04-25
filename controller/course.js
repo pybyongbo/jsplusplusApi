94aafae96743a9de2538351e0c1bf2267d80a42b
pybyongbo-pc <pyb901214@163.com>
2022-04-25 22:09:17 +0800

    新增课程分类编辑/删除接口

--- controller/course.js
+++ controller/course.js
@@ -4,7 +4,7 @@ const courseModel = require("../lib/mysql.js");
 const fdata = async (data) => {
 
   const promises = data.map(async item => {
-    const listItem = await courseModel.get_field_course_list(item.fieldType,'').then(res => res.length);
+    const listItem = await courseModel.get_field_course_list(item.fieldType, '').then(res => res.length);
     return listItem;
 
   });
@@ -16,9 +16,7 @@ const fdata = async (data) => {
     ...item,
     totalCount: listItems[index]
   }));
-
   return res;
-
 }
 
 
@@ -47,10 +45,10 @@ exports.getFieldCourse = async ctx => {
 // 查询分类对应的列表数据:
 
 exports.getFieldCourseList = async ctx => {
-  let { field = -1,keywords="" } = ctx.query;
-  console.log('keywords',keywords);
+  let { field = -1, keywords = "" } = ctx.query;
+  console.log('keywords', keywords);
   await courseModel
-    .get_field_course_list(field,keywords)
+    .get_field_course_list(field, keywords)
     .then(result => {
       ctx.body = {
         code: 0,
@@ -68,35 +66,71 @@ exports.getFieldCourseList = async ctx => {
 
 //新增课程分类信息  courseCateCreate
 exports.courseCateCreate = async ctx => {
-    let { fieldName, fieldType } = ctx.request.body;
-    await courseModel.add_course_cate([fieldName, fieldType]).then(() => {
-        ctx.body = {
-          code: 0,
-          message: "课程分类新增成功"
-        };
-      })
-      .catch(() => {
-        ctx.body = {
-          code: 500,
-          message: "新增失败"
-        };
-      });
-    }
-
-
-// 新增课程信息
-exports.courseCreate = async ctx => {
-  let { title, fieldType, thumb,price,studying } = ctx.request.body;
-  await courseModel.add_course_info([title, fieldType, thumb,price,studying]).then(() => {
-      ctx.body = {
-        code: 0,
-        message: "课程信息新增成功"
-      };
-    })
+  let { fieldName, fieldType } = ctx.request.body;
+  await courseModel.add_course_cate([fieldName, fieldType]).then(() => {
+    ctx.body = {
+      code: 0,
+      message: "课程分类新增成功"
+    };
+  })
     .catch(() => {
       ctx.body = {
         code: 500,
         message: "新增失败"
       };
     });
-  }
+}
+
+
+//更新课程分类信息  courseCateUpdate
+exports.courseCateUpdate = async ctx => {
+  let { fieldName, id } = ctx.request.body;
+  await courseModel.update_course_cate([fieldName, id]).then(() => {
+    ctx.body = {
+      code: 0,
+      message: "课程分类更新成功"
+    };
+  })
+    .catch(() => {
+      ctx.body = {
+        code: 500,
+        message: "更新失败"
+      };
+    });
+}
+
+// 删除课程分类信息 courseCateDelete
+
+exports.courseCateDelete = async ctx => {
+  let { fieldName, id } = ctx.request.body;
+  await courseModel.course_cate_delete([id]).then(() => {
+    ctx.body = {
+      code: 0,
+      message: "课程分类删除成功"
+    };
+  })
+    .catch(() => {
+      ctx.body = {
+        code: 500,
+        message: "删除失败"
+      };
+    });
+}
+
+
+// 新增课程信息
+exports.courseCreate = async ctx => {
+  let { title, fieldType, thumb, price, studying } = ctx.request.body;
+  await courseModel.add_course_info([title, fieldType, thumb, price, studying]).then(() => {
+    ctx.body = {
+      code: 0,
+      message: "课程信息新增成功"
+    };
+  })
+    .catch(() => {
+      ctx.body = {
+        code: 500,
+        message: "新增失败"
+      };
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const controller = require('../controller/course.js')
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!',
-    message:'by bibilili jsplusplus api'
+    message: 'by bibilili jsplusplus api'
   })
 })
 
@@ -23,8 +23,14 @@ router.get('/course/get_course_fields', controller.getFieldCourse);
 //课程分类列表数据
 router.get('/course/get_courses/all', controller.getFieldCourseList);
 
-// 新增课程信息
+// 新增课程分类信息
 router.post('/course/course_cate_create', controller.courseCateCreate);
+
+// 修改课程分类信息
+router.post('/course/course_cate_update', controller.courseCateUpdate);
+
+// 删除课程分类信息
+router.post('/course/course_cate_delete', controller.courseCateDelete);
 
 // 新增课程信息
 router.post('/course/course_create', controller.courseCreate);
